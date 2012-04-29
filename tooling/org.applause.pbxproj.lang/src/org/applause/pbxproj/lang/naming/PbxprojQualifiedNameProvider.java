524913d6c6d80fd8fd8b7b397a90929847bc9c97
Natalia Ossipova <natalia.ossipova@t-online.de>
2012-04-30 01:28:18 +0200

    Added cross-references to build files and frameworks in the Pbxproj grammar

--- tooling/org.applause.pbxproj.lang/src/org/applause/pbxproj/lang/naming/PbxprojQualifiedNameProvider.java
+++ tooling/org.applause.pbxproj.lang/src/org/applause/pbxproj/lang/naming/PbxprojQualifiedNameProvider.java
@@ -1,13 +1,28 @@
 package org.applause.pbxproj.lang.naming;
 
+import org.applause.pbxproj.lang.pbxproj.BuildConfiguration;
+import org.applause.pbxproj.lang.pbxproj.BuildFileReference;
+import org.applause.pbxproj.lang.pbxproj.FileReference;
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.eclipse.xtext.naming.QualifiedName;
+
+import com.google.inject.Inject;
 
 public class PbxprojQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider {
 
-	// @Inject
-	// IQualifiedNameConverter qualifiedNameConverter;
-	//
-	// public QualifiedName qualifiedName(BuildFile obj) {
-	// return qualifiedNameConverter.toQualifiedName(obj.getName());
-	// }
+	@Inject
+	IQualifiedNameConverter qualifiedNameConverter;
+
+	public QualifiedName qualifiedName(FileReference obj) {
+		return qualifiedNameConverter.toQualifiedName(obj.getName());
+	}
+
+	public QualifiedName qualifiedName(BuildFileReference obj) {
+		return qualifiedNameConverter.toQualifiedName(obj.getName());
+	}
+
+	public QualifiedName qualifiedName(BuildConfiguration obj) {
+		return qualifiedNameConverter.toQualifiedName(obj.getName());
+	}
 }
